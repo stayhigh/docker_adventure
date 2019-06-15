287ef4d3f2cb520b9231a0479baa0e931c6a1f7d
stayhigh <redhero0702@gmail.com>
2019-06-15 16:28:02 +0000

    [release] docker create+docker start=docker run, -t option is a must for docker create command

--- lifecycle_of_docker_container/lifecycle_test.sh
+++ lifecycle_of_docker_container/lifecycle_test.sh
@@ -12,9 +12,11 @@ then
     docker rm ${container_name} && sleep 1
 fi
 
-#container_id=$(docker create --name ${container_name} ${image} | head -c 12)
-docker run -d -it --name ${container_name} ${image}
+# NOTE: -t option is a must, it will not show "UP" without -t option
+container_id=$(docker create --name ${container_name} -t -i ubuntu:18.04 bash | head -c 12)
+# create and run new container: docker run -d -it --name ${container_name} ${image}
 
+docker start "${container_name}"
 docker stop "${container_name}"
 docker start "${container_name}"
 
