295fbb8fe812ac36df199467e3648d16e39af8ae
stayhigh <redhero0702@gmail.com>
2019-06-19 15:59:41 +0000

    [release] script for install docker compose

---
+++ install_dockercompose/install_dockercompose.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+### https://github.com/docker/compose/releases
+
+set -x 
+curl -L https://github.com/docker/compose/releases/download/1.25.0-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
